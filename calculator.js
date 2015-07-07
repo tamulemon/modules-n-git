e1c89293a2329dd66c0982a232af70a257c234f3
Meng Chen <tamulemon@gmail.com>
2015-07-07 16:04:35 -0700

    add multiplication

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 exports.addition = function(x, y) {
   return x + y;
 };
+exports. multiplication = function () {
+	var result = arguments[0];
+	for (var i = 1; i < arguments.length; i++) {
+		result = result * arguments[i];
+	}
+	return result;
+}
+
